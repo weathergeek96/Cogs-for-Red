3b78d097c1366bf4aae6f3b756b15a1a2119370c
Caleb Chapman <cchapman0069@kctcs.edu>
2017-02-17 09:01:02 -0500

    Update jsontest.py

--- jsontest.py
+++ jsontest.py
@@ -1,5 +1,4 @@
 import os
-import asyncio
 from discord.ext import commands
 from .utils.dataIO import dataIO
 import aiohttp
@@ -26,9 +25,10 @@ class json:
     async def json(self, ctx):
         """gets JSON file and announces the data from Nookipedia Discord API"""
         search = "https://nookipedia.com/network/discord-api.php?action=get&user_id=191340394485645312&user_display=Jake"
-        async with aiohttp.get(search) as r:
-            result = await r.json()
-            await self.bot.say("INFORMATION!!! " + result['user_id'])
+        async with aiohttp.ClientSession() as session:
+            async with session.get(search) as r:
+                result = await r.json()
+                await self.bot.say("INFORMATION!!! " + result['user_id'])
             
 
 
